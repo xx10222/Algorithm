422afbcb7dee6c68aa704a7fccb5004486c89812
xx10222 <yjw4960@gmail.com>
2022-07-11 12:05:24 +0900

    :sparkles: [main] 프로그래머스 DP 1번 - N으로 표현

---
+++ src/main/java/programmers/DP1.java
@@ -0,0 +1,49 @@
+package programmers;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class DP1 {
+    public static void main(String[] args) {
+        int N = 5;
+        int number = 12;
+        System.out.println(solution(N, number));
+    }
+    public static int solution(int N, int number) {
+        int answer = -1;
+        int now = N;
+        Set<Integer>[] setArr = new HashSet[9];
+        for(int i=1;i<9;i++) {
+            setArr[i] = new HashSet<>();
+            setArr[i].add(now);
+            now = now*10 + N;
+        }
+
+        for(int i=1;i<9;i++) {
+            for(int j=1;j<i;j++) {
+                for(Integer a : setArr[j]) {
+                    for(Integer b : setArr[i-j]) {
+                        setArr[i].add(a+b);
+                        setArr[i].add(a-b);
+                        setArr[i].add(b-a);
+                        setArr[i].add(a*b);
+                        if(a!=0) {
+                            setArr[i].add(b/a);
+                        }
+                        if (b!=0) {
+                            setArr[i].add(a/b);
+                        }
+                    }
+                }
+            }
+        }
+
+        for(int i=1;i<9;i++) {
+            if(setArr[i].contains(number)) {
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+}
