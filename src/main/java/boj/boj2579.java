88b662faad2aa212b7980793d7e7fd91c9ae4225
xx10222 <yjw4960@gmail.com>
2022-09-27 14:50:40 +0900

    :sparkles: [백준] 2579번 - 계단 오르기

---
+++ src/main/java/boj/boj2579.java
@@ -0,0 +1,27 @@
+package boj;
+
+import java.util.Scanner;
+
+public class boj2579 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        int[] dp = new int[n+1];
+        int[] arr = new int[n+1];
+
+        for(int i=1;i<=n;i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        dp[1] = arr[1];
+        dp[2] = arr[1]+arr[2];
+        dp[3] = Math.max(arr[1], arr[2]) + arr[3];
+
+        for(int i=4;i<=n;i++) {
+            dp[i] = Math.max(dp[i-3]+dp[i-1], dp[i-2]) + arr[i];
+        }
+
+        System.out.println(dp[n]);
+    }
+}
