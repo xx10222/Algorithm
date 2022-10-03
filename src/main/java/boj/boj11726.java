4967090882cdbb25d9af7a935f9f90ec002e3064
xx10222 <yjw4960@gmail.com>
2022-10-03 18:17:01 +0900

    :sparkles: [백준] 11726번 - 2xn 타일링

--- src/main/java/boj/boj11726.java
+++ src/main/java/boj/boj11726.java
@@ -12,7 +12,7 @@ public class boj11726 {
         dp[2]=2;
 
         for(int i=3;i<=n;i++) {
-            dp[i] = dp[i-1]+dp[i-2];
+            dp[i] = (dp[i-1]+dp[i-2])%10007;
         }
 
         System.out.println(dp[n]);
