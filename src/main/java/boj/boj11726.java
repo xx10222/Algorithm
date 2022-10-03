c06212181713e9ec38855a47098bc0906af7f1cc
xx10222 <yjw4960@gmail.com>
2022-10-03 18:11:07 +0900

    :sparkles: [백준] 11726번 - 2xn 타일링

---
+++ src/main/java/boj/boj11726.java
@@ -0,0 +1,21 @@
+package boj;
+
+import java.util.Scanner;
+
+public class boj11726 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] dp = new int[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+
+        for(int i=3;i<=n;i++) {
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+
+        System.out.println(dp[n]);
+
+    }
+}
