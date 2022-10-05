27e3025348b9832d5e594c55b1875ddcde636453
xx10222 <yjw4960@gmail.com>
2022-10-05 16:45:55 +0900

    :sparkles: [백준] 2193번 - 이친수

--- src/main/java/boj/boj2193.java
+++ src/main/java/boj/boj2193.java
@@ -6,13 +6,19 @@ public class boj2193 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int[][] dp = new int[n+1][2];
+        long[][] dp = new long[n+1][2];
 
+        dp[1][0] = 0;
         dp[1][1] = 1;
 
         for(int i=2;i<=n;i++) {
-            dp[i][0] = dp[i-1][0] + dp[i-1][1];
-            dp[i][1] = dp[i-1][0];
+            for(int j=0;j<2;j++) {
+                if(j==0) {
+                    dp[i][j] = dp[i-1][0] + dp[i-1][1];
+                } else {
+                    dp[i][j] = dp[i-1][0];
+                }
+            }
         }
 
         System.out.println(dp[n][0]+dp[n][1]);
