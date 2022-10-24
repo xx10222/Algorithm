ae94847a1a2c45cc66ce080e522e26eb0e8d2213
xx10222 <yjw4960@gmail.com>
2022-10-24 18:13:20 +0900

    :sparkles: [백준] 2743번 - 단어 길이 재기

---
+++ src/main/java/boj/boj2743.java
@@ -0,0 +1,11 @@
+package boj;
+
+import java.util.Scanner;
+
+public class boj2743 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        System.out.println(s.length());
+    }
+}
