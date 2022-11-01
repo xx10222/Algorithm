c974af42608abcaa1049c58da06b3aff9aed86b3
xx10222 <yjw4960@gmail.com>
2022-11-01 19:41:39 +0900

    :sparkles: [구름 먼데이 챌린지] 1주차 1번 해설

--- src/main/java/groom/monday1_1.java
+++ src/main/java/groom/monday1_1.java
@@ -1,16 +1,19 @@
 package groom;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class monday1_1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int answer = 1;
         int[] arr = new int[n];
         for(int i=0;i<n;i++) {
-//            arr[i] = scanner.nextInt();
-            answer *= scanner.nextInt();
+            arr[i] = scanner.nextInt();
+        }
+        BigInteger answer = BigInteger.ONE;
+        for(int i=0;i<n;i++) {
+            answer = answer.multiply(BigInteger.valueOf(arr[i]));
         }
         System.out.println(answer);
 
