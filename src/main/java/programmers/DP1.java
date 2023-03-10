f7e188f1d9da240a03d1f867ffc4854060077c2d
xx10222 <yjw4960@gmail.com>
2023-03-10 19:09:28 +0900

    :zap: [프로그래머스] DP - N으로 표현 : 분기 조건 추가를 통한 시간 효율성 개선

--- src/main/java/programmers/DP1.java
+++ src/main/java/programmers/DP1.java
@@ -9,37 +9,43 @@ public class DP1 {
         int number = 12;
         System.out.println(solution(N, number));
     }
+
     public static int solution(int N, int number) {
         int answer = -1;
         int now = N;
         Set<Integer>[] setArr = new HashSet[9];
-        for(int i=1;i<9;i++) {
+        for (int i = 1; i < 9; i++) {
             setArr[i] = new HashSet<>();
             setArr[i].add(now);
-            now = now*10 + N;
+            now = now * 10 + N;
         }
 
-        for(int i=1;i<9;i++) {
-            for(int j=1;j<i;j++) {
-                for(Integer a : setArr[j]) {
-                    for(Integer b : setArr[i-j]) {
-                        setArr[i].add(a+b);
-                        setArr[i].add(a-b);
-                        setArr[i].add(b-a);
-                        setArr[i].add(a*b);
-                        if(a!=0) {
-                            setArr[i].add(b/a);
+        for (int i = 1; i < 9; i++) {
+            for (int j = 1; j < i; j++) {
+                for (Integer a : setArr[j]) {
+                    for (Integer b : setArr[i - j]) {
+                        setArr[i].add(a + b);
+                        setArr[i].add(a * b);
+
+                        if(a>b) {
+                            setArr[i].add(a - b);
+                        } else {
+                            setArr[i].add(b - a);
                         }
-                        if (b!=0) {
-                            setArr[i].add(a/b);
+
+                        if (a != 0) {
+                            setArr[i].add(b / a);
+                        }
+                        if (b != 0) {
+                            setArr[i].add(a / b);
                         }
                     }
                 }
             }
         }
 
-        for(int i=1;i<9;i++) {
-            if(setArr[i].contains(number)) {
+        for (int i = 1; i < 9; i++) {
+            if (setArr[i].contains(number)) {
                 answer = i;
                 break;
             }
